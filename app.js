8f0c8b0c40f0bbf330b643cac2e9cdd6c19c61cf
José Muñoz Ruiz <jarmunozr@gmail.com>
2017-01-08 17:50:42 -0300

    proteccion de rutas para creacion de movie sin autenticacion

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var config = require('./lib/config');
 
 var app = express();
 
+//mongoose.connect(config.database);
+mongoose.createConnection(config.database);
+
+var auth_middleware = require('./lib/middleware/auth')
 var auth = require('./routes/auth')
 var index = require('./routes/index')
 var movie = require('./routes/movie')
@@ -20,10 +26,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// rutas inseguras
 app.use('/', index)
 app.use('/auth', auth)
-app.use('/movie', movie)
 app.use('/user', user)
+
+// Middleware
+app.use(auth_middleware)
+
+// rutas seguras
+app.use('/movie', movie)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
